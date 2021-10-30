16238f442c9376c8f82600de04878e646c8e3f2f
rdtejeda <roberto.daniel.tejeda@gmail.com>
2021-10-30 14:48:21 +0000

    30oct14:20

--- Sab_Puntero/src/Sab_Puntero.c
+++ Sab_Puntero/src/Sab_Puntero.c
@@ -25,41 +25,63 @@ Retornando la cantidad de reemplazos o -1 en caso de error.
 #include <ctype.h>
 // https://www.onlinegdb.com/eY9VEBPKo sabado 30!0
 
-int main()
+int funcionBuscarYReemplazar(int* pEntero,int* tamano, int valor);
+int main(void) {
+
+	int* pAuxiliar;
+	int tamanoPunteros;
+
+	tamanoPunteros=5;
+	pAuxiliar=(int*)malloc(sizeof(int)*tamanoPunteros);
+
+
+	if(pAuxiliar!=NULL)
+	{
+		for(int i=0;i<tamanoPunteros ;i++)
+		{
+			*(pAuxiliar+i)=i+1;
+		}
+	}
+
+	for(int i=0;i<tamanoPunteros ;i++)
+	{
+		printf("%d\n",*(pAuxiliar+i));
+	}
+
+	funcionBuscarYReemplazar(pAuxiliar,&tamanoPunteros, 2);
+
+	for(int i=0;i<tamanoPunteros ;i++)
+	{
+		printf("%d\n",*(pAuxiliar+i));
+	}
+
+	return EXIT_SUCCESS;
+}
+
+int funcionBuscarYReemplazar(int* pEntero,int* tamano, int valor)
 {
-    char cadenaDeChars[51] = "sabado";
-    /*
-    char* p;
-    p = cadenaDeChars;
-    printf("%d %d", sizeof(p), sizeof(cadenaDeChars));
-    */
-    int reemplazos;
-    reemplazos = ReemplazarCaracteres (cadenaDeChars, sizeof(cadenaDeChars), 'a', 'i');
-    printf("Cant. reemplazos: %d\nPalabra reemplazada: %s", reemplazos, cadenaDeChars);
+	int retorno=-1;
+	if(pEntero!=NULL)
+	{
+		for(int i=0;i<*tamano ;i++)
+		{
+			if(*(pEntero+i)==valor)
+			{
+				for(int j=i;j<*tamano-1;j++)
+				{
+					*(pEntero+j)=*(pEntero+j+1);
+				}
+				retorno=0;
+				pEntero=(int*)realloc(pEntero,sizeof(int)*(*tamano-1));
+				*tamano=*tamano-1;
+				break;
+			}
 
-    return 0;
+		}
+	}
+
+	return retorno;
 }
-
-int ReemplazarCaracteres (char* cadena, int tamanioCadena, char caracterAReemplazar, char caracterQueVaAReemplazar) {
-    int cantidadReemplazos;
-    int longitudDeCadena;
-
-    cantidadReemplazos = -1;
-
-    if (cadena != NULL) {
-        cantidadReemplazos = 0;
-        longitudDeCadena = strnlen(cadena, tamanioCadena);
-        for (int i = 0; i < longitudDeCadena; i++) {
-            if (*(cadena+i) == caracterAReemplazar)
-            {
-                *(cadena+i) = caracterQueVaAReemplazar;
-                cantidadReemplazos++;
-            }
-        }
-    }
-    return cantidadReemplazos;
-}
-
 
 /*
 int ReemplazarCaracteres (char* cadena, int tamanioCadena, char caracterAReemplazar, char caracterQueVaAReemplazar);
